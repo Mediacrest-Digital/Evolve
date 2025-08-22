2e95876f5f2515978bc0886aad4e82d3fbdcb897
Alex Kimeu <alexkimeu1999@gmail.com>
2025-08-22 12:27:48 +0300

    fix: implement scroll-to-top behavior on route navigation
    - Use requestAnimationFrame with instant behavior to prevent visible scrolling
    - Resolves issue where new pages would inherit scroll position from previous page
    - Ensures all page transitions start from the top of the page

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,38 @@ import Services from "./pages/Services";
 import CareTeam from "./pages/CareTeam";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+    });
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => (
-    <TooltipProvider>
-      <Toaster />
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/care-team" element={<CareTeam />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </TooltipProvider>
+  <TooltipProvider>
+    <Toaster />
+    <BrowserRouter>
+      <ScrollToTop />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/care-team" element={<CareTeam />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  </TooltipProvider>
 );
 
 export default App;
